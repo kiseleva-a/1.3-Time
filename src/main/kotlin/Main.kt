f6977ebbf24f708cb763b9c78831167092875962
Arina Kiseleva <arinaskiseleva@gmail.com>
2023-08-27 13:35:43 +0300

    change Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,17 +5,18 @@ fun main(args: Array<String>) {
     val activityCheck: String = agoToText(inputTime)
     println(activityCheck)
 }
-fun agoToText(inputTime: Int):String = when (inputTime) {
-    in 0..60 -> "был(а) только что"
-    61, 21 * 60, 31 * 60, 41 * 60, 51 * 60 -> {"был(а) " + inputTime/60 + " минуту назад"}
-    in 2 * 60..4 * 60 -> {"был(а) " + inputTime/60 + " минуты назад"}
-    in 5 * 60..1 * 60 * 60 -> {"был(а) " + inputTime/60 + " минут назад"}
-    1 * 60 * 60 + 1, 21 * 60 * 60 -> {"был(а) " + inputTime/60/60 + " час назад"}
-    in 2 * 60 * 60..4 * 60 * 60 -> {"был(а) " + inputTime/60/60 + " часа назад"}
-    in 5 * 60 * 60..20 * 60 * 60  -> {"был(а) " + inputTime/60/60 + " часов назад"}
-    in 22 * 60 * 60..24 * 60 * 60 -> {"был(а) " + inputTime/60/60 + " часа назад"}
-    in 24 * 60 * 60 + 1..48 * 60 * 60 -> "был(а) вчера"
-    in 48 * 60 * 60 + 1..72 * 60 * 60 -> "был(а) позавчера"
+fun agoToText(inputTime: Int):String = when {
+    inputTime in 0..60 -> "был(а) только что"
+    inputTime / 60 % 10 == 1 && inputTime /60 % 10 != 11 -> {"был(а) " + inputTime/60 + " минуту назад"}
+    inputTime / 60 % 10 in 2..4 && inputTime / 60 % 10 !in 12..14 -> {"был(а) " + inputTime/60 + " минуты назад"}
+    inputTime in 5 * 60.. 60 * 60 -> {"был(а) " + inputTime/60 + " минут назад"}
+    inputTime == 1 * 60 * 60 + 1 || inputTime == 21 * 60 * 60 -> {"был(а) " + inputTime/60/60 + " час назад"}
+    inputTime in 2 * 60 * 60..4 * 60 * 60 -> {"был(а) " + inputTime/60/60 + " часа назад"}
+    inputTime in 5 * 60 * 60..20 * 60 * 60  -> {"был(а) " + inputTime/60/60 + " часов назад"}
+    inputTime in 22 * 60 * 60..24 * 60 * 60 -> {"был(а) " + inputTime/60/60 + " часа назад"}
+    inputTime in 24 * 60 * 60 + 1..48 * 60 * 60 -> "был(а) вчера"
+    inputTime in 48 * 60 * 60 + 1..72 * 60 * 60 -> "был(а) позавчера"
     else -> "был(а) давно"
 
 }
+
